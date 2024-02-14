e294ec8425fcb41efb79cb2a7d00555d0fd0bbbc
DataSperling <simon.francis.mrsc@tutanota.de>
2024-02-14 16:09:21 +0100

    modfied resource loading

--- audio-preprocessor/audio_file_reader.py
+++ audio-preprocessor/audio_file_reader.py
@@ -1,4 +1,9 @@
+import errno
+import os
 import yaml
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class audio_file_reader:
@@ -6,12 +11,12 @@ class audio_file_reader:
 
 
 # Read appropriate file path from config
-def read_resource(yml_config, audio_dataset):
+def read_resource(yml_config, training_audio):
   with open(yml_config, 'r') as stream:
     try:
       data = yaml.safe_load(stream)
-      if audio_dataset in data['paths']:
-        path = (data['paths'])[audio_dataset]
+      if training_audio in data['paths']:
+        path = (data['paths'])[training_audio]
       else:
         print('audio dataset not found...')
     except yaml.YAMLError as exception:
@@ -21,4 +26,19 @@ def read_resource(yml_config, audio_dataset):
 
 # Load appropriate audio dataset
 def load_resource(path):
-  read_resource('config.yml', 'test_audio')
+  right_chan = []
+  try:
+    audio = wave.open(path)
+    sig_arr = np.frombuffer(audio.readframes(audio.getnframes()), dtype=np.int16)
+    if audio.getnchannels() != 1:
+      right_chan = sig_arr[1::2]
+    else:
+      right_cha = sig_arr
+  except:
+    raise FileNotFoundError(
+      errno.ENOENT, os.strerror(errno.ENOENT), path)
+  return right_chan
+
+
+
+load_resource(read_resource('config.yml', 'test_audio'))
